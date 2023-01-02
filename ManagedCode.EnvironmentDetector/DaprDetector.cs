d10cb3ef34f7f2986da12108e3bee863e08b451f
ksemenenko <mail@ksemenenko.com>
2023-01-02 18:40:17 +0100

    env fix

--- ManagedCode.EnvironmentDetector/DaprDetector.cs
+++ ManagedCode.EnvironmentDetector/DaprDetector.cs
@@ -4,8 +4,8 @@ public static class DaprDetector
 {
     static DaprDetector()
     {
-        IsInDapr = Environment.GetEnvironmentVariables().Cast<KeyValuePair<string,string>?>()
-            .Any(k => k.HasValue && k.Value.Key.ToUpperInvariant().Contains("DAPR_"));
+        var x = Environment.GetEnvironmentVariables().Keys.Cast<string>()
+            .Any(k => k.ToUpperInvariant().Contains("DAPR_"));
     }
 
     public static bool IsInDapr { get; }
--- ManagedCode.EnvironmentDetector/KubernetesDetector.cs
+++ ManagedCode.EnvironmentDetector/KubernetesDetector.cs
@@ -4,8 +4,8 @@ public static class KubernetesDetector
 {
     static KubernetesDetector()
     {
-        IsInKubernetes = Environment.GetEnvironmentVariables().Cast<KeyValuePair<string,string>?>().
-            Any(k => k.HasValue && k.Value.Key.ToUpperInvariant().Contains("KUBERNETES_"));
+        IsInKubernetes = Environment.GetEnvironmentVariables().Keys.Cast<string>()
+            .Any(k => k.ToUpperInvariant().Contains("KUBERNETES_"));
     }
 
     public static bool IsInKubernetes { get; }
